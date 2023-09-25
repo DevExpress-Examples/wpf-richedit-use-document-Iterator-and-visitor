27a4af792b5cbc24febd40c49092ae74a8a56471
DevExpressExampleBot <fakeEmail>
2023-09-25 20:26:50 +0400

    Source auto update [skip ci]

--- VB/DocumentIteratorExample/MainWindow.xaml.vb
+++ VB/DocumentIteratorExample/MainWindow.xaml.vb
@@ -1,67 +1,61 @@
-﻿Imports DevExpress.XtraRichEdit.API.Native
-Imports System
-Imports System.Collections.Generic
-Imports System.Linq
+Imports DevExpress.XtraRichEdit.API.Native
 Imports System.Text
-Imports System.Threading.Tasks
 Imports System.Windows
 Imports System.Windows.Controls
-Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Input
-Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Navigation
-Imports System.Windows.Shapes
 
 Namespace DocumentIteratorExample
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits Window
 
-		Private Sub SimpleButton_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			Dim visitor As New MyVisitor()
-			Dim iterator As New DocumentIterator(richEditControl1.Document, True)
-			Do While iterator.MoveNext()
-				iterator.Current.Accept(visitor)
-			Loop
-			textEdit1.Text = visitor.Text
-		End Sub
-	End Class
-	Public Class MyVisitor
-		Inherits DocumentVisitorBase
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
 
-'INSTANT VB NOTE: The field buffer was renamed since Visual Basic does not allow fields to have the same name as other class members:
-		Private ReadOnly buffer_Conflict As StringBuilder
-		Public Sub New()
-			Me.buffer_Conflict = New StringBuilder()
-		End Sub
-		Protected ReadOnly Property Buffer() As StringBuilder
-			Get
-				Return buffer_Conflict
-			End Get
-		End Property
-		Public ReadOnly Property Text() As String
-			Get
-				Return Buffer.ToString()
-			End Get
-		End Property
+        Private Sub SimpleButton_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            Dim visitor As MyVisitor = New MyVisitor()
+            Dim iterator As DocumentIterator = New DocumentIterator(Me.richEditControl1.Document, True)
+            While iterator.MoveNext()
+                iterator.Current.Accept(visitor)
+            End While
 
-		Public Overrides Sub Visit(ByVal text As DocumentText)
-			Dim prefix As String = If(text.TextProperties.FontBold, "**", "")
+            Me.textEdit1.Text = visitor.Text
+        End Sub
+    End Class
 
-			Buffer.Append(prefix)
-			Buffer.Append(text.Text)
-			Buffer.Append(prefix)
-		End Sub
-		Public Overrides Sub Visit(ByVal paragraphEnd As DocumentParagraphEnd)
-			Buffer.AppendLine()
-		End Sub
-	End Class
+    Public Class MyVisitor
+        Inherits DocumentVisitorBase
+
+        Private ReadOnly bufferField As StringBuilder
+
+        Public Sub New()
+            bufferField = New StringBuilder()
+        End Sub
+
+        Protected ReadOnly Property Buffer As StringBuilder
+            Get
+                Return bufferField
+            End Get
+        End Property
+
+        Public ReadOnly Property Text As String
+            Get
+                Return Buffer.ToString()
+            End Get
+        End Property
+
+        Public Overrides Sub Visit(ByVal text As DocumentText)
+            Dim prefix As String = If(text.TextProperties.FontBold, "**", "")
+            Buffer.Append(prefix)
+            Buffer.Append(text.Text)
+            Buffer.Append(prefix)
+        End Sub
+
+        Public Overrides Sub Visit(ByVal paragraphEnd As DocumentParagraphEnd)
+            Buffer.AppendLine()
+        End Sub
+    End Class
 End Namespace
